153c1eb5d14534993494e110c0b3bbf0808877d5
rea <hi@rea.ink>
2022-06-02 16:49:02 +0800

    fix: dayjs plugin

--- src/plugins/dayjs.ts
+++ src/plugins/dayjs.ts
@@ -1,6 +1,6 @@
 import dayjs from 'dayjs'
 import isLeapYear from 'dayjs/plugin/isLeapYear'
-import LocalizedFormat from 'dayjs/plugin/LocalizedFormat'
+import LocalizedFormat from 'dayjs/plugin/localizedFormat'
 import 'dayjs/locale/zh-cn'
 
 dayjs.extend(isLeapYear)
